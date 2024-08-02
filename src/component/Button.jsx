e763b410d356a44821aabbe9f9ee3e88a0263978
segun olowoyeye <segunolowoyeye@gmail.com>
2024-08-02 23:05:17 +0100

    fiveth commit

--- src/component/Button.jsx
+++ src/component/Button.jsx
@@ -3,7 +3,7 @@ import React from "react";
 function Button({ onClickHandler, value, title }) {
   return (
     <button
-      className="shadow-lg border-black px-3"
+      className="shadow-lg border-black px-3 bg-inputColor"
       onClick={onClickHandler}
       value={value}
     >
--- src/component/Card.jsx
+++ src/component/Card.jsx
@@ -4,15 +4,15 @@ import { BsFillBagHeartFill } from "react-icons/bs";
 
 function Card({ img, title, star, reviews, newPrice, prevPrice }) {
   return (
-    <section className="mt-10  space-y-7 shadow-lg border-black p-1 mx-auto">
-      <div className=" h-[135px] lg:h-[155px] flex items-center justify-cente">
+    <section className="mt-10  space-y-5 shadow-lg border-black p-1 mx-auto">
+      <div className=" h-[75px] flex items-center justify-center">
         <img
           src={img}
           alt={title}
           className="max-h-full w-auto bg-inputColor"
         />
       </div>
-      <div className="card-details text-[13px] p-4 bg-slate-300">
+      <div className="card-details text-[15px] p-4">
         <h3 className="ml-1">{title}</h3>
         <div className="flex space-x-2">
           <div className="flex text-yellow-500">
@@ -21,7 +21,6 @@ function Card({ img, title, star, reviews, newPrice, prevPrice }) {
             {star}
             {star}
           </div>
-          <span>{reviews}</span>
         </div>
 
         <div className="card-price flex space-x-5">
