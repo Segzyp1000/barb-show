7a5df7e89f76bf683d3dbe49b8dfc8c109936746
segun olowoyeye <segunolowoyeye@gmail.com>
2024-11-06 04:15:43 +0100

    addedede

--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -34,81 +34,83 @@ const Register = () => {
   };
 
   return (
-    <form type="POST" className="py-12">
-      <div className="border-b border-gray-900/10 p-10 m-auto w-4/5 bg-white contain container pb-12 ">
-        <p className="text-3xl font-bold text-gray-900">Sign up</p>
-        <h1 className=" font-md leading-7 text-gray-900 mt-20">
-          Fill your information to register
-        </h1>
-        <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
-          <div className="sm:col-span-3">
-            <label
-              htmlFor="first-name"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Username
-            </label>
-            <div className="mt-2">
-              <input
-                type="text"
-                name="first-name"
-                id="first-name"
-                autoComplete="given-name"
-                onChange={(e) => setUsername(e.target.value)}
-                className="block w-full rounded-md bg-inputColor border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
+    <div className="w-full py-24">
+      <form type="POST" className="py-16 pb-16">
+        <div className="border-b border-gray-900/10 p-10 m-auto w-3/5 bg-white container">
+          <p className="text-3xl font-bold text-gray-900">Sign up</p>
+          <h1 className=" font-md leading-7 text-gray-900 mt-20">
+            Fill your information to register
+          </h1>
+          <div className="mt-10 grid grid-cols-1 gap-x-6 gap-y-8 sm:grid-cols-6">
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="first-name"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Username
+              </label>
+              <div className="mt-2">
+                <input
+                  type="text"
+                  name="first-name"
+                  id="first-name"
+                  autoComplete="given-name"
+                  onChange={(e) => setUsername(e.target.value)}
+                  className="block w-full rounded-md bg-inputColor border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="email"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Email address
+              </label>
+              <div className="mt-2">
+                <input
+                  id="email"
+                  onChange={(e) => setEmail(e.target.value)}
+                  name="email"
+                  type="email"
+                  autoComplete="email"
+                  className="block w-full rounded-md bg-inputColor border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
+            </div>
+
+            <div className="sm:col-span-3">
+              <label
+                htmlFor="password"
+                className="block text-sm font-medium leading-6 text-gray-900"
+              >
+                Password
+              </label>
+              <div className="mt-2">
+                <input
+                  type="password"
+                  onChange={(e) => setPassword(e.target.value)}
+                  name="password"
+                  id="password"
+                  autoComplete="password"
+                  className="block w-full rounded-md bg-inputColor border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                />
+              </div>
             </div>
           </div>
 
-          <div className="sm:col-span-3">
-            <label
-              htmlFor="email"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Email address
-            </label>
-            <div className="mt-2">
-              <input
-                id="email"
-                onChange={(e) => setEmail(e.target.value)}
-                name="email"
-                type="email"
-                autoComplete="email"
-                className="block w-full rounded-md bg-inputColor border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
-
-          <div className="sm:col-span-3">
-            <label
-              htmlFor="password"
-              className="block text-sm font-medium leading-6 text-gray-900"
-            >
-              Password
-            </label>
-            <div className="mt-2">
-              <input
-                type="password"
-                onChange={(e) => setPassword(e.target.value)}
-                name="password"
-                id="password"
-                autoComplete="password"
-                className="block w-full rounded-md bg-inputColor border-0 py-1.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-              />
-            </div>
-          </div>
+          {error && <p className="text-red-700">{error}</p>}
+          <button
+            onClick={handleSubmit}
+            type="submit"
+            className="text-white bg-navColor px-2 rounded-xl mb-12 hover:slate mt-10"
+          >
+            Register
+          </button>
         </div>
-
-        {error && <p className="text-red-700">{error}</p>}
-        <button
-          onClick={handleSubmit}
-          type="submit"
-          className="text-white bg-navColor px-2 rounded-xl mb-12 hover:slate mt-10"
-        >
-          Register
-        </button>
-      </div>
-    </form>
+      </form>
+    </div>
   );
 };
 export default Register;
